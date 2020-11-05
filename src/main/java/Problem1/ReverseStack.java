c0a2646da2ceedd21abe281b88018316717c55c6
SirAJn8r <hyperactive@live.com>
2020-11-04 19:41:39 -0800

    Finished reverseStack, only Permutation left

--- src/main/java/Problem1/ReverseStack.java
+++ src/main/java/Problem1/ReverseStack.java
@@ -6,10 +6,25 @@ public class ReverseStack
 {
     public static void reverseStack(Stack<Integer> stack)
     {
-        if(stack.empty())
-            return;
+        if(stack.isEmpty()) return;
+
         int temp = stack.pop();
         reverseStack(stack);
-        stack.push(temp);
+
+        reverseHelper(temp, stack);
+    }
+
+    //puts temp at bottom of stack
+    private static void reverseHelper(int temp, Stack<Integer> stack)
+    {
+        if(stack.isEmpty())
+        {
+            stack.push(temp);
+            return;
+        }
+
+        int top = stack.pop();
+        reverseHelper(temp, stack);
+        stack.push(top);
     }
 }
