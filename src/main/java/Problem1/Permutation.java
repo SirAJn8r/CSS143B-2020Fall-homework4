37d8d52c6e2914d513744c475f6072457cb8c983
SirAJn8r <hyperactive@live.com>
2020-11-07 15:24:31 -0800

    Finished Permutation finally

--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -5,88 +5,36 @@ import java.util.List;
 
 public class Permutation
 {
-    /*public static List<List<Integer>> permute(int[] nums)
-    {
-        List<Integer> newNums = new ArrayList<>();
-        for(int i : nums)
-            newNums.add(i);
-        return makePermute(newNums);
-    }
-
-    private static List<List<Integer>> makePermute(List<Integer> nums)
-    {
-        if(nums.size() == 1)
-        {
-            List<List<Integer>> temp = new ArrayList<>();
-            temp.add(nums);
-            return temp;
-        }
-        if(nums.size() == 2)
-        {
-            List<List<Integer>> toRet = new ArrayList<>();
-            toRet.add(nums);
-            List<Integer> toAdd = new ArrayList<Integer>();
-            toAdd.add(nums.get(1));
-            toAdd.add(nums.get(0));
-            toRet.add(toAdd);
-            return toRet;
-        }
-
-        List<List<Integer>> toRet = new ArrayList<>();
-        for(int i = 0; i < nums.size(); i++)
-        {
-            List<List<Integer>> temp = makePermute(removeIndex(nums, i));
-            for(int j = 0; j < temp.size(); j++)
-            {
-                toRet.add(addToFront(temp.get(j), nums.get(i)));
-            }
-        }
-        return toRet;
-    }
-
-    private static List<Integer> removeIndex(List<Integer> data, int i)
-    {
-        List<Integer> list = new ArrayList<>();
-        for(int j = 0; j < data.size(); j++)
-        {
-            if(i == j) continue;
-            list.add(data.get(j));
-        }
-        return list;
-    }
-
-    */
-
     public static List<List<Integer>> permute(int[] nums)
     {
         List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) { result.add(new ArrayList<Integer>()); return result; }
         permute(nums, 0, result);
         return result;
     }
 
-    public static void permute(int[] nums, int position, List<List<Integer>> result)
+    private static void permute(int[] nums, int pos, List<List<Integer>> result)
     {
-        //stopping condition
-        if(position == nums.length-1)
+        if(pos == nums.length-1)
         {
-            List<Integer> oneResult = new ArrayList<>();
+            List<Integer> baseCase = new ArrayList<>();
             for(int i = 0; i < nums.length; i++)
-            {
-                oneResult.add(nums[i]);
-            }
+                baseCase.add(nums[i]);
+            result.add(baseCase);
             return;
         }
-        // put number at position in the front
-        // call the same private permute with position+1
-        // put the number back to its original position
+        for(int i = pos; i < nums.length; i++)
+        {
+            swap(nums, i, pos);
+            permute(nums, pos+1, result);
+            swap(nums, i, pos);
+        }
     }
 
-    private static List<Integer> addToFront(List<Integer> data, int val)
+    private static void swap(int[] nums, int i, int j)
     {
-        List<Integer> list = new ArrayList<>();
-        list.add(val);
-        for(Integer i : data)
-            list.add(i);
-        return list;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
